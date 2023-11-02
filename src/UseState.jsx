66818479d112043bc9c7ea020787335ce196a737
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-11-02 16:25:26 +0100

    :sparkles: FEAT useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,18 +1,41 @@
+import { useState } from 'react'
+
 function UseState() {
+	const [count, setCount] = useState(0)
+	const [user, setUser] = useState({
+		name: 'John',
+		age: 22,
+	})
 
-  let count = 0
+	const increment = () => {
+		setCount(count + 1)
+		setUser({ ...user, age: user.age + 1 })
+	}
 
-  const increment = () => {
-    count++
-    console.log(count)
-  }
+	// const increment = () => {
+	//   setCount(count + 1)
+	//   setCount(count + 1)
+	//   setCount(count + 1)
+	// }
 
-  return <>
-    <h1>UseState</h1>
-    <p>Count: {count}</p>
-    <button onClick={increment}>Increment</button>
-    <hr />
-  </>
+	// const incrementState = () => {
+	//   setCount((count) => count+ 1)
+	//   setCount((count) => count+ 1)
+	//   setCount((count) => count+ 1)
+	// }
+
+	return (
+		<>
+			<h1>UseState</h1>
+			<p>Count: {count}</p>
+			<p>
+				Age de {user.name} : {user.age}
+			</p>
+			<button onClick={increment}>Increment</button>
+			{/* <button onClick={incrementState}>Increment State</button> */}
+			<hr />
+		</>
+	)
 }
 
 export default UseState
